244ed23f6e482c486417735073dded7e59cc4c5e
Jane Reynolds <jane.e.reynolds@gmail.com>
2015-10-19 15:12:41 -0700

    article index, view indiv post

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,14 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+  
+  def show
+    @article = Article.find(params[:id])
+  end
+  
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
+end
