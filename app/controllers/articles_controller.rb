e3ff5b98527427cb2b5ac0c307ee76559e99579d
Jane Reynolds <jane.e.reynolds@gmail.com>
2015-10-19 19:07:49 -0700

    added single post read, update and delete functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper
+  
   def index
     @articles = Article.all
   end
@@ -7,8 +9,32 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
   
-  private
-    def article_params
-      params.require(:article).permit(:title, :body)
-    end
+  def new
+    @article = Article.new
+  end
+  
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to article_path(@article)
+  end
+  
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    
+    flash.notice = "Article '#{@article.title}' updated!"
+    
+    redirect_to article_path(@article)
+  end
+  
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
 end
